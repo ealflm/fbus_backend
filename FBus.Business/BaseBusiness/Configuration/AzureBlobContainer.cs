436ea314d2510e574622eb89cad7918d8cddf639
danhnlc <danhnlc252k@gmail.com>
2022-10-06 16:06:27 +0700

    fix index Azureblobcontainer

--- FBus.Business/BaseBusiness/Configuration/AzureBlobContainer.cs
+++ FBus.Business/BaseBusiness/Configuration/AzureBlobContainer.cs
@@ -2,8 +2,8 @@ namespace FBus.Business.BaseBusiness.Configuration
 {
     public enum AzureBlobContainer
     {
-        Admin = 1,
-        Student = 2,
-        Driver = 3
+        Admin,
+        Student,
+        Driver,
     }
 }
